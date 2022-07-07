f2c34ff968a3e4ed345cf555be10428836367bea
Spandana Chinnappa <spandanachinnappa@gmail.com>
2022-07-07 16:24:57 +0530

    Footer compoment test cases

---
+++ src/compoments/__tests__/footer.test.js
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/react";
+import Footer from "../footer"
+
+describe("footer", () => {
+  test("Footer contains copyright" , () =>{
+    render(<Footer/>);
+    const footer = screen.getByTestId("footer");
+  })
+});
--- src/compoments/footer.js
+++ src/compoments/footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-           <p>Copyright &#169; 2011-2022 Sabka Bazaar Grocery Supplies Pvt Ltd</p>
+        <footer className="footer"data-testid="footer" >
+           <p>Copyright &#169; 2011-{new Date().getFullYear()} Sabka Bazaar Grocery Supplies Pvt Ltd</p>
         </footer>
     )
 }
